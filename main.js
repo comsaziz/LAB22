0e5beb85da5025a08dead41a94a52dfe981a2397
comsaziz <comsaziz@gmail.com>
2024-09-09 14:46:37 +0300

    Add Arithmetic operations

--- main.js
+++ main.js
@@ -1,27 +1,63 @@
 // Q1
-var numbers = [1,5,3,8,2,4,6,7,9,10];
-var numbers1 = numbers.sort((a, b) => a - b);
-console.log(numbers1);
+// var numbers = [1,5,3,8,2,4,6,7,9,10];
+// var numbers1 = numbers.sort((a, b) => a - b);
+// console.log(numbers1);
 
-var numbers = [1,5,3,8,2,4,6,7,9,10];
-let newnum = numbers.map(num => num * num);
-console.log(newnum);
+// var numbers = [1,5,3,8,2,4,6,7,9,10];
+// let newnum = numbers.map(num => num * num);
+// console.log(newnum);
 
 
-let number3 = numbers.find(num => num > 25 ) 
-console.log(number3)
+// let number3 = numbers.find(num => num > 25 ) 
+// console.log(number3)
         
     
-let number4 = numbers.every(num => num >5)
-console.log(number4);
+// let number4 = numbers.every(num => num >5)
+// console.log(number4);
 
-let number5 = numbers.every(num => num = 3)
-console.log(number5);
+// let number5 = numbers.every(num => num = 3)
+// console.log(number5);
 
-let number6 = numbers.filter(num => num % 2 === 0)
-console.log(number6);
+// let number6 = numbers.filter(num => num % 2 === 0)
+// console.log(number6);
 
-let number7 = Math.floor(numbers.length / 2);
-let number8 = numbers.splice (number7- 1, 2);
-console.log(number8);
+// let number7 = Math.floor(numbers.length / 2);
+// let number8 = numbers.splice (number7- 1, 2);
+// console.log(number8);
 
+
+
+
+
+
+
+
+
+function Add(num1, num2){
+    return num1 + num2;
+}
+console.log(Add(20,5))
+
+
+
+function Mins(num1, num2){
+    return num1 - num2;
+}
+console.log(Mins(20,5))
+
+
+function mul(num1, num2){
+    return num1 * num2;
+}
+console.log(mul(20,5))
+
+
+function div(num1, num2){
+    return num1 / num2;
+}
+console.log(div(20,5))
+
+function per(num1, num2){
+    return num1 % num2;
+}
+console.log(per(20,2))
